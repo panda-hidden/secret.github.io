99aef05883a84d7eb69946c4551d385f2491f24e
panda-hidden <MC123panda321@gmail.com>
2025-06-30 18:18:51 -0400

    Test dynamic text

--- wotd.js
+++ wotd.js
@@ -5,9 +5,11 @@ input.addEventListener("keypress", function(e) {
         //e.preventDefault();
         console.log(Winput.value);
         if (Winput.value === "inspector"){
+            document.getElementById('wotd-out').innerHTML = 'Nice!';
             console.log("Nice!");
         }
         else{
+            document.getElementById('wotd-out').innerHTML = 'Not a word of the day, try again!';
             console.log("Not a word of the day!");
         }
     }
