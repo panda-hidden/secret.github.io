e0db7fcfffa3e42abbf38f99099358ca5d2b083d
panda-hidden <MC123panda321@gmail.com>
2025-06-30 18:27:49 -0400

    hash update

--- wotd.js
+++ wotd.js
@@ -5,14 +5,12 @@ input.addEventListener("keypress", function(e) {
         //e.preventDefault();
         console.log(Winput.value);
         let input_hash = md5(Winput.value);
-        console.log(input_hash);
-        if (Winput.value === "inspector"){
+        if (input_hash == '8e96c1fb87ac069c2a39f1ed61b10428'){
             document.getElementById('wotd-out').innerHTML = 'Nice!';
-            console.log("Nice!");
         }
         else{
             document.getElementById('wotd-out').innerHTML = 'Not a word of the day, try again!';
-            console.log("Not a word of the day!");
+            //console.log("Not a word of the day!");
         }
     }
 });
