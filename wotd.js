38cebee0a66dbdfef02bd98b8f16b6b82810a8d7
panda-hidden <MC123panda321@gmail.com>
2025-06-30 18:09:56 -0400

    testing json

--- wotd.js
+++ wotd.js
@@ -4,5 +4,11 @@ input.addEventListener("keypress", function(e) {
     if (e.key === "Enter") {
         //e.preventDefault();
         console.log(Winput.value);
+        if (Winput.value === "inspector"){
+            console.log("Nice!");
+        }
+        else{
+            console.log("Not a word of the day!");
+        }
     }
 });
