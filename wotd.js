9d019352948e74c3d11c583c1f506c9c0812e439
panda-hidden <MC123panda321@gmail.com>
2025-06-30 17:37:17 -0400

    Test Input

--- wotd.js
+++ wotd.js
@@ -1,2 +1,7 @@
-let textRead = Winput.value;
-console.log(textRead);
+var input = document.getElementById("Winput");
+input.addEventListener("keypress", function(event2) {
+    if (event2.key === "Enter") {
+        event2.preventDefault();
+        console.log(Winput.value);
+    }
+});
