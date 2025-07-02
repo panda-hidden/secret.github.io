de0afca73c0216914ee9a868acde67166961c34a
panda-hidden <MC123panda321@gmail.com>
2025-07-02 02:06:07 -0400

    bold check

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var output = document.getElementById("firstp");
 
 function secret(event) {
-    var update = "You guys <b>f<b/>igured out the first clue<b>!</b> <br> Something is wrong here though. <br> We all need to be on the right page. Luckily, some of us in PBI have shared something in common with me that will help us get there. <br> It is important to stay present, but since we change over time, for the next puzzle don’t forget the past.  <br> Once you figure out all four answers, just add them up!"
+    var update = "You guys <b>f</b>igured out the first clue<b>!</b> <br> Something is wrong here though. <br> We all need to be on the right page. Luckily, some of us in PBI have shared something in common with me that will help us get there. <br> It is important to stay present, but since we change over time, for the next puzzle don’t forget the past.  <br> Once you figure out all four answers, just add them up!"
     output.innerHTML = update;
 }
