0927bb18d3a9eb1c55b81cc7a96bd6731ea487f5
panda-hidden <MC123panda321@gmail.com>
2025-07-02 02:04:02 -0400

    dynamic string

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function coordinate(event) {
-    let x = event.clientX;
-    let y = event.clientY;
-    console.log(x, y);
+var output = document.getElementById("firstp");
+
+function secret(event) {
+    var update = "You guys figured out the first clue <b> ! </b> <br> Something is wrong here though. <br> We all need to be on the right page. Luckily, some of us in PBI have shared something in common with me that will help us get there. <br> It is important to stay present, but since we change over time, for the next puzzle don’t forget the past.  <br> Once you figure out all four answers, just add them up!"
+    output.innerHTML = update;
 }
