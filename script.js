6d00855976c1c64684788273d04e5144e28d9e99
panda-hidden <MC123panda321@gmail.com>
2025-07-02 02:17:00 -0400

    bolding

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var output = document.getElementById("firstp");
 
 function secret(event) {
-    var update = "You guys <b>f</b>igured out the first clue<b>!</b> <br> Something is wrong here though. <br> We all need to be on the right page. Luckily, some of us in PBI have shared something in common with me that will help us get there. <br> It is important to stay present, but since we change over time, for the next puzzle don’t forget the past.  <br> Once you figure out all four answers, just add them up!"
+    var update = "You guys <b>f</b>ig<b>u</b>red ou<b>t</b> the f<b>i</b>rst clue<b>!</b> <br> <b>S</b>o<b>m</b>et<b>h</b>ing is <b>w</b>rong here th<b>o</b>ugh<b>.</b> <br> We a<b>l</b>l need to be on the right page. Luckily, som<b>e</b> of us in <b>P</b>BI ha<b>v</b>e sh<b>a</b>red something in common with me that will help us get the<b>r</b>e. <br> It is important to stay present, but since we <b>c</b>han<b>g</b>e over time, for the next pu<b>z</b>zle <b>d</b>on’t forget the past.  <br> Once you figure out all four answers, just add them up!"
     output.innerHTML = update;
 }
