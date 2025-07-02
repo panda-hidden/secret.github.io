0ee4daae6f0177c557d87e8266bff955bb82ce89
panda-hidden <MC123panda321@gmail.com>
2025-07-02 02:27:29 -0400

    fakes

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 var output = document.getElementById("firstp");
 
-function secret(event) {
+//Multiple so you can't just read the right one in inspect elements
+function secret1(event) {
+    var update = "You guys fig<b>u</b>red ou<b>t</b> the <b>f</b>irst clue! <br> <b>S</b>o<b>m</b>et<b>h</b>ing <b>i</b>s <b>w</b>rong here th<b>o</b>ugh<b>.</b> <br> We a<b>l</b>l need to <b>b</b>e on the right page. Luc<b>k</b>il<b>y</b>, som<b>e</b> of us in <b>P</b>BI ha<b>v</b>e sh<b>a</b>red something in common with me that will help us get the<b>r</b>e. <br> It is important to stay present, but since we <b>c</b>han<b>g</b>e over time, for the next pu<b>z</b>zle <b>d</b>on’t forget the past.  <br> O<b>n</b>ce you figure out all four answers<b>,</b> <b>j</b>ust add them up!"
+    output.innerHTML = update;
+}
+function secret2(event) {
+    var update = "You guys figured ou<b>t</b> the first cl<b>u</b>e! <br> <b>S</b>o<b>m</b>et<b>h</b>ing <b>i</b>s wrong here th<b>o</b>ugh<b>.</b> <br> We all need to <b>b</b>e on the right page. Luc<b>k</b>il<b>y</b>, som<b>e</b> of us in <b>P</b>BI ha<b>v</b>e sh<b>a</b>red something in common with me that will help us get the<b>r</b>e. <br> It is important to stay present, but since we <b>c</b>han<b>g</b>e over time, for the next pu<b>z</b>zle <b>d</b>on’t forget the past.  <br> O<b>n</b>ce you figure out all four answers<b>,</b> <b>j</b>ust add them up!"
+    output.innerHTML = update;
+}
+function secret3(event) {
+    var update = "You guys <b>f</b>ig<b>u</b>red ou<b>t</b> the first clue! <br> <b>S</b>o<b>m</b>et<b>h</b>ing <b>i</b>s <b>w</b>rong here th<b>o</b>ugh<b>.</b> <br> We a<b>l</b>l need to <b>b</b>e on the right page. Lu<b>c</b><b>k</b>il<b>y</b>, som<b>e</b> of us in <b>P</b>BI ha<b>v</b>e sh<b>a</b>red something in common with me that will help us get the<b>r</b>e. <br> It is important to stay present, but since we chan<b>g</b>e over time, for the next pu<b>z</b>zle <b>d</b>on’t forget the past.  <br> O<b>n</b>ce you figure out all four answers<b>,</b> <b>j</b>ust add them up!"
+    output.innerHTML = update;
+}
+function secret8(event) {
     var update = "You guys <b>f</b>ig<b>u</b>red ou<b>t</b> the f<b>i</b>rst clue! <br> <b>S</b>o<b>m</b>et<b>h</b>ing is <b>w</b>rong here th<b>o</b>ugh<b>.</b> <br> We a<b>l</b>l need to <b>b</b>e on the right page. Luc<b>k</b>il<b>y</b>, som<b>e</b> of us in <b>P</b>BI ha<b>v</b>e sh<b>a</b>red something in common with me that will help us get the<b>r</b>e. <br> It is important to stay present, but since we <b>c</b>han<b>g</b>e over time, for the next pu<b>z</b>zle <b>d</b>on’t forget the past.  <br> O<b>n</b>ce you figure out all four answers<b>,</b> <b>j</b>ust add them up!"
     output.innerHTML = update;
 }
